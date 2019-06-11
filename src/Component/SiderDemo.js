c9e372457193e8604e80ce4d9d773366d9bb26ca
Chalermchon <chalermchon.o@ku.th>
2019-06-12 00:01:07 +0700

    (C) little refactor

--- src/Component/SiderDemo.js
+++ src/Component/SiderDemo.js
@@ -1,20 +1,20 @@
-
-import React from 'react';
+import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import '../cssFile/SiderDemo.css';
 import AllQuestion from './AllQuestion';
 import Field from './Field';
-import { Layout, Menu, Icon,  Drawer, Button } from 'antd';
+import { Layout, Icon, Drawer, Button } from 'antd';
+import SubjectsMenu from './SubjectsMenu';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer , Sider} = Layout;
 
-class SiderDemo extends React.Component{
-  constructor(){
+export class SiderDemo extends Component {
+  constructor() {
     super();
     this.state = {
       visible: false,
 
-      tag : ""
+      tag: ""
     }
   }
   showDrawer = () => {
@@ -37,67 +37,46 @@ class SiderDemo extends React.Component{
     // })
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
+        <Header style={{ background: '#fff', padding: 0, marginLeft: 0 }} >
+          <div style={{ marginLeft: 20, fontSize: 20 }}>
+            APP NAME
+          </div>
+        </Header>
+
+
         <Layout>
           <Sider
             breakpoint="lg"
             collapsedWidth="0"
             onBreakpoint={broken => {
-              console.log(broken);
+                console.log(broken);
             }}
             onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
+                console.log(collapsed, type);
             }}
           >
-              <div className="logo">
-                 
+            <SubjectsMenu/>
+          </Sider>
+          
+          <Layout>
+            
+            <Content style={{ margin: '24px 16px 0' }}>
+              <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>
+                {/* <i class="fas fa-biohazard fa-5x"></i> */}
+                <AllQuestion />
+                <Button type="primary" onClick={this.showDrawer}>
+                  <Icon type="plus" /> add new question
+                      </Button>
               </div>
+            </Content>
 
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} >
-
-                <Menu.Item key="math" onClick={this.handleButon}>
-                  <Icon type="file-excel" />
-                  <span className="nav-text" >math</span>
-                </Menu.Item>
-                <Menu.Item key="science" onClick={this.handleButon}>
-                <Icon type="thunderbolt" />
-                  <span className="nav-text">science</span>
-                </Menu.Item>
-                <Menu.Item key="language" onClick={this.handleButon}>
-                <Icon type="flag" />
-                  <span className="nav-text">language</span>
-                </Menu.Item>
-                <Menu.Item key="programming" onClick={this.handleButon}>
-                  <Icon type="code" />
-                  <span className="nav-text">programming</span>
-                </Menu.Item>
-
-              </Menu>
-            </Sider>
-
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0, marginLeft: 0 }} >
-                <div style={{  marginLeft: 20, fontSize: 20 }}>
-                    APP NAME
-                </div>
-              </Header>
-
-              <Content style={{ margin: '24px 16px 0' }}>
-                <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>
-                  {/* <i class="fas fa-biohazard fa-5x"></i> */}
-                  <AllQuestion/>
-                  <Button type="primary" onClick={this.showDrawer}>
-                    <Icon type="plus" /> add new question
-                  </Button>
-
-                </div>
-              </Content>
-              
-              <Footer style={{ textAlign: 'center' }}>footer</Footer>
-            </Layout>
+            <Footer style={{ textAlign: 'center' }}>footer</Footer>
           </Layout>
+
+        </Layout>
 
 
         <Drawer
@@ -106,9 +85,9 @@ class SiderDemo extends React.Component{
           onClose={this.onClose}
           visible={this.state.visible}
         >
-          <Field/>
-          <div  
-            style = {{            
+          <Field />
+          <div
+            style={{
               position: 'absolute',
               left: 0,
               bottom: 0,
@@ -118,15 +97,13 @@ class SiderDemo extends React.Component{
               background: '#fff',
               textAlign: 'right',
             }}
-          >             
+          >
           </div>
         </Drawer>
 
       </div>
-      
-    );
+    )
   }
 }
 
-
-export default SiderDemo
+export default SiderDemo;
---
+++ src/Component/SubjectsMenu.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Menu from 'antd/lib/menu'
+import Icon from 'antd/lib/icon'
+
+export class SubjectsMenu extends Component {
+    render() {
+        return (
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} >
+
+                <Menu.Item key="math" onClick={this.handleButon}>
+                    <Icon type="file-excel" />
+                    <span className="nav-text" >math</span>
+                </Menu.Item>
+                <Menu.Item key="science" onClick={this.handleButon}>
+                    <Icon type="thunderbolt" />
+                    <span className="nav-text">science</span>
+                </Menu.Item>
+                <Menu.Item key="language" onClick={this.handleButon}>
+                    <Icon type="flag" />
+                    <span className="nav-text">language</span>
+                </Menu.Item>
+                <Menu.Item key="programming" onClick={this.handleButon}>
+                    <Icon type="code" />
+                    <span className="nav-text">programming</span>
+                </Menu.Item>
+
+            </Menu>
+        )
+    }
+}
+
+export default SubjectsMenu;
