cc7714686c43a8f266267f5ddb123605983425bd
Kitpavin <Chaiwong>
2019-07-03 15:20:29 +0700

    little commit

--- src/Component/AllQuestion.js
+++ src/Component/AllQuestion.js
@@ -3,6 +3,7 @@ import { Tag, Card, Button, Modal, Steps, message, Row, Col } from 'antd';
 // import 'antd/dist/antd.css';
 import '../cssFile/AllQuestion.css';
 import { connect } from 'react-redux';
+const { Step } = Steps;
 // import DoQuiz from './DoQuiz';
 
 const { fire } = require('../redux-firebase/firebaseControl');
@@ -24,6 +25,7 @@ class AllQuestion extends React.Component {
             visible: false,
             current: 0,
             aQuiz: [],
+            checkReasonPage: false,
         }
     }
 
@@ -59,18 +61,27 @@ class AllQuestion extends React.Component {
         this.setState({ current });
     }
 
+    onSelectChoice = (e) => {
+        console.log(e.target.value);
+        
+    }
+
+    showReason = () => {
+        console.log("click show reason");
+        
+    }
+
     showModal = (id) => {
+        console.log(this.state.checkReasonPage);
+        
         let aQuiz = [];
         db.collection(`User/user3/Quiz/${id}/Questions`).get()
             .then((res) => {
                 res.forEach(doc => {
-                    console.log(doc.data());
                     aQuiz.push(doc.data())
+                    console.log(doc.id);
                 })
-                console.log(aQuiz)
-                // console.log("question : " + question);
                 aQuiz.map((item) => {
-                    console.log(item);
                     var question = item.question
                     var correctChoice = item.correctChoice
                     var choices = item.choices
@@ -79,16 +90,18 @@ class AllQuestion extends React.Component {
                         {
                             content:
                                 <div>
-                                    {question}<br />
+                                    <div className="box">
+                                        {question}
+                                    </div>
                                     {/* {correctChoice} */}
-                                    <div style={{ marginTop: "100px" }}>
+                                    <div style={{ marginTop: "10px" }}>
                                         <Row gutter={8} style={{ width: "565px" }}>
-                                            <Col span={10}><Button className="bottonChoice">{choices[0]}</Button></Col>
-                                            <Col span={10}><Button className="bottonChoice">{choices[1]}</Button></Col>
+                                            <Col span={10}><button value="1" onClick={this.onSelectChoice} className="bottonChoice">{choices[0]}</button></Col>
+                                            <Col span={10}><button value="2" onClick={this.onSelectChoice} className="bottonChoice">{choices[1]}</button></Col>
                                         </Row>
                                         <Row gutter={8} style={{ width: "565px" }}>
-                                            <Col span={10}><Button className="bottonChoice">{choices[2]}</Button></Col>
-                                            <Col span={10}><Button className="bottonChoice">{choices[3]}</Button></Col>
+                                            <Col span={10}><button value="3" onClick={this.onSelectChoice} className="bottonChoice">{choices[2]}</button></Col>
+                                            <Col span={10}><button value="4" onClick={this.onSelectChoice} className="bottonChoice">{choices[3]}</button></Col>
                                         </Row>
                                     </div>
 
@@ -116,7 +129,6 @@ class AllQuestion extends React.Component {
     }
 
     render() {
-        console.log(this.state.current)
         const { current } = this.state;
         // console.log(this.state.allData)
         var listOfQuestion = this.state.allData.map((val, index) => {
@@ -132,9 +144,11 @@ class AllQuestion extends React.Component {
             var component =
                 <Card
                     hoverable
-                    style={{ width: '95%' }}
-                    title={topic}
-                    extra={[<Button type="primary" onClick={() => this.showModal(id)}>start doing !!</Button>]}
+                    style = {{ width: '95%' }}
+                    title = {topic}
+                    extra = {[
+                        <Button type="primary" onClick={() => this.showModal(id)}>start doing !!</Button>
+                    ]}
                 >
                     {tag} <br />
                     type => {type}<br />
@@ -149,12 +163,16 @@ class AllQuestion extends React.Component {
         return (
             <div>
                 <Modal
-                    visible = {this.state.visible}
-                    onOk = {this.handleOk}
-                    onCancel = {this.handleCancel}
-                    footer = {null}
-                    bodyStyle = {{width:"500px"}}
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                    style={{}}
+                    styteBody={{height: "500px"}}
                 >
+                    <Steps current={current}>
+                        {steps.map(item => (<Step key={item.title} title={item.title} />))}
+                    </Steps>
                     {
                         steps[current] !== undefined &&
                         (
@@ -182,6 +200,7 @@ class AllQuestion extends React.Component {
                                 Previous
                             </Button>
                         )}
+                        <Button style={{ marginLeft: 8 }} onClick={this.showReason}>Show reason</Button>
                     </div>
                 </Modal>
                 {/* <DoQuiz/> */}
